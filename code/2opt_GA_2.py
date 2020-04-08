1cc4ad8f093f94f205549ca5e23ca6f999815934
Gwang Jong Ko <rhkswhdwkd@naver.com>
2020-04-08 17:54:26 +0900

    Update 2opt_GA_2.py

--- code/2opt_GA_2.py
+++ code/2opt_GA_2.py
@@ -2,15 +2,27 @@ import math
 import numpy as np
 import random
 import timeit
-
 from threading import Thread
 import functools
 
+dist_ar = [] # 거리표(global)
+# limit_time = 36 # 제한시간(global)
+cities_count = 0 # 도시 수(global)
+dots_list = [] # 도시 리스트(global)
+
+# Hyper Parameter
+limits = (60) * 36 # 제한시간
+MUT = 0.2 # 변이확률
+SEL = 0.85 # 선택압
+chrCOUNT = 50 # 해집단 내 염색체 개수
+selCOUNT = 25 # selection시 선택되는 상위 염색체의 개수
+
+# 시간제한 데코레이터
 def timeout(seconds_before_timeout):
     def deco(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            res = [Exception('function [%s] timeout [%s seconds] exceeded!' % (func.__name__, seconds_before_timeout))]
+            res = [Exception('function [%s] timeout [%s seconds] exceeded!' %(func.__name__, seconds_before_timeout))]
             def newFunc():
                 try:
                     res[0] = func(*args, **kwargs)
@@ -30,18 +42,6 @@ def timeout(seconds_before_timeout):
             return ret
         return wrapper
     return deco
-
-dist_ar = [] # 거리표(global)
-limit_time = 36 # 제한시간(global)
-cities_count = 0 # 도시 수(global)
-dots_list = [] # 도시 리스트(global)
-
-# Hyper Parameter
-limits = 36 # 제한시간
-MUT = 0.2 # 변이확률
-SEL = 0.85 # 선택압
-chrCOUNT = 50 # 해집단 내 염색체 개수
-selCOUNT = 25 # selection시 선택되는 상위 염색체의 개수
 
 # 거리표 제작(param : 문제 경로) : dist_df
 def make_distDataframe(str):
@@ -99,22 +99,28 @@ def optFunc(stri) :
 
     return newArr
 
-@timeout(limits)
+# 0 ~ ranges-1의 범위 중 두 개를 랜덤으로 샘플링해서 list 리턴
+def randomTwo(ranges) :
+    randomList = []
+    randomList += random.sample(range(0,ranges), 2)
+    randomList.sort()
+    return randomList
+
 def TSP_GA() :
     # 환경 설정 및 초기화
     generation = 0  # 현재 세대
-    population = [] # temp chromosome
-    population_fit = [] # temp fitness
-    # step_result = [] # step을 거칠 때 변화된 population
+    chromosome = [] # temp chromosome
+    chromosome_fit = [] # temp fitness
 
     # initialize
     for i in range(chrCOUNT) :
-        population.append(optFunc(random.sample(range(0, cities_count), cities_count)))
+        # 2opt 이용해서 좀 더 좋은 initial chromosome 설정
+        chromosome.append(optFunc(random.sample(range(0, cities_count), cities_count)))
 
     for i in range(chrCOUNT) :
-        population_fit.append(round(cal_fit(population[i]), 5))
+        chromosome_fit.append(round(cal_fit(chromosome[i]), 5))
 
-    populations = np.array([population, population_fit])
+    populations = np.array([chromosome, chromosome_fit])
     populations = populations.T
     # print('초기 염색체 : \n', population, '\n염색체 별 적합도 :\n', population_fit)
     # print(populations)
@@ -134,40 +140,37 @@ def TSP_GA() :
             # 선택난수가 선택압보다 작으면 두 유전자 중 좋은 유전자가 선택. 아니면 반대로
             parents_index = [0]*2
             for i in range(len(parents_index)):
-                firGeneNum = random.randrange(0, chrCOUNT - endSel - 1)
-                secGeneNum = random.randrange(firGeneNum + 1, chrCOUNT - endSel)
+                selGeneNum = randomTwo((chrCOUNT-endSel))
                 match = random.random()
                 if match < SEL :
-                    if populations[firGeneNum,1] < populations[secGeneNum,1] :
-                        parents_index[i] = firGeneNum
+                   if populations[selGeneNum[0],1] < populations[selGeneNum[1],1] :
+                        parents_index[i] = selGeneNum[0]
+                   else:
+                        parents_index[i] = selGeneNum[1]
+                else :
+                    if populations[selGeneNum[0],1] < populations[selGeneNum[1],1] :
+                        parents_index[i] = selGeneNum[1]
                     else:
-                        parents_index[i] = secGeneNum
-                else:
-                    if populations[firGeneNum,1] < populations[secGeneNum,1] :
-                        parents_index[i] = secGeneNum
-                    else:
-                        parents_index[i] = firGeneNum
+                        parents_index[i] = selGeneNum[0]
             # crossover : order-based crossover
             daddy_value = populations[parents_index[0], 0].copy()
             mommy_value = populations[parents_index[1], 0].copy()
-            headCSLine = random.randrange(0, cities_count-1)
-            tailCSLine = random.randrange(headCSLine+1, cities_count)
-            offspring = daddy_value[headCSLine: tailCSLine]
-            for i in daddy_value[headCSLine : tailCSLine] :
+            CsGeneNum = randomTwo(cities_count)
+            offspring = daddy_value[CsGeneNum[0] : CsGeneNum[1]]
+            for i in daddy_value[CsGeneNum[0] : CsGeneNum[1]] :
                 mommy_value.remove(i)
             for i in range(len(offspring)) :
-                mommy_value.insert(headCSLine+i, offspring[i])
+                mommy_value.insert(CsGeneNum[0]+i, offspring[i])
             offspring = mommy_value
             offspring_fit = cal_fit(offspring)
 
             # mutation : exchange mutation
             mut_p = random.random()
             if mut_p < MUT :
-                headPoint = random.randrange(0, cities_count-1)
-                tailPoint = random.randrange(headPoint+1, cities_count)
-                mut_Temp = offspring[headPoint]
-                offspring[headPoint] = offspring[tailPoint]
-                offspring[tailPoint] = mut_Temp
+                MtGeneNum = randomTwo(cities_count)
+                mut_Temp = offspring[MtGeneNum[0]]
+                offspring[MtGeneNum[0]] = offspring[MtGeneNum[1]]
+                offspring[MtGeneNum[1]] = mut_Temp
                 offspring_fit = cal_fit(offspring)
             populations = np.vstack((populations, [offspring, offspring_fit]))
         # Replacement
@@ -176,11 +179,14 @@ def TSP_GA() :
             np.delete(populations, (chrCOUNT+i), axis=0)
         print(generation, '세대 최적 해 : \n', populations[0,0],"\n", populations[0,1])
 
+@timeout(limits)
+def start_GA(stri) :
+    make_distDataframe(stri)
+    TSP_GA()
 
 try :
     start = timeit.default_timer()
-    make_distDataframe("2opt_dots/2opt_cycle100.in")
-    TSP_GA()
+    start_GA("2opt_dots/2opt_cycle100.in")
     stop = timeit.default_timer()
     print(stop - start)
 except :
@@ -194,13 +200,12 @@ except :
 2. population 전체에 2opt를 실행하니 오히려 수렴을 못하는 결과 발생... 
     하위 *selCount개의 chromosome에 2opt를 시행했더니 결과가 훨씬 좋았음.
     2opt를 거친다고해서 fitness가 무조건 좋아지는 것은 아닌것은 알겠는데 어떻게 적용해야 가장 최적일까???
+    ex) 하위 몇 %만 적용, 적용과 미적용을 비교해서 좋은 경우만 대치
 
 //이슈
 1. python에서 thread로 함수를 중간에 죽이는게 windows에서 일반적으로 불가. 삽질 너무 많이 했음...ㅠ
-    https://cnpnote.tistory.com/entry/PYTHON-%EC%93%B0%EB%A0%88%EB%93%9C%EB%A5%BC-%EC%A3%BD%EC%9D%BC-%EB%B0%A9%EB%B2%95%EC%9D%B4-%EC%9E%88%EC%8A%B5%EB%8B%88%EA%B9%8C
     데코레이터(?)를 만들고 Thread.join (timeout) 메서드를 사용. 
     죽이는 것은 아니라서 백그라운드에서 실행 유지.
 2. 특정 범위 내의 중복되지 않는 두 개의 랜덤을 골라내는 것에도 삽질 많이함.
-    샘플링의 방법이 있었으나 적합하지는 않아서 while 이용해서 온전한 두 개를 뽑을 때까지 random을 수행하게 했음. 
 3. 코드를 좀 수정해서 모듈화를 진행했으나 교수님이 지도해주시기엔 좋지 못한 것 같아서 다시 합침... 
 '''

4fe4acdf0dad4e99243d004f3d44ee641131b5f6
Gwang Jong Ko <rhkswhdwkd@naver.com>
2020-08-14 17:00:08 +0900

    Add files via upload

--- code/GA/geneticAlgorithmOnSch.py
+++ code/GA/geneticAlgorithmOnSch.py
@@ -6,13 +6,14 @@ import random
 class GA :
     def __init__(self, populationSize, selectionProbability, threshold, mutationProbability):
         self.job = ['j1', 'j2']
-        self.operation = [['o11', 'o12'],
+        self.operation = [['o11', 'o22'],
                           ['o21', 'o22', 'o23']]
-        self.machine = [[2, 999, 3, 4, 999],
-                        [6, 8, 999, 6, 7],
-                        [5, 999, 6, 5, 11],
-                        [3, 4, 999, 999, 5],
-                        [4, 999, 5, 999, 8]]
+        self.machine = {'o11' : [2, 6, 5, 3, 4],
+                        'o12' :[999, 8, 999, 4, 999],
+                        'o21' : [3, 999, 6, 999, 5],
+                        'o22' :[4, 6, 5, 999, 999],
+                        'o23' : [999, 7, 11, 5, 8]}
+        self.operations = 5
         self.populationSize = populationSize
         self.selectionProbability = selectionProbability
         self.mutationProbability = mutationProbability
@@ -21,35 +22,46 @@ class GA :
         self.fitness = []
         self.population = [[], []]
 
-    def getFitness(self, schedule) :
+    def getFitness(self, chromosome) :
         fitness = 0
+        ms = chromosome[0]
+        os = chromosome[1]
+        temp_oper = self.operation
+        real_os = []
+        real_pTime = [0, 0, 0, 0, 0]
+
+        for o in range(self.operations) :
+            real_os.append(temp_oper[os[o]-1].pop(0))
+
+        for o in range(real_os) :
+            real_ms = [0, 0]
+            for i in range(real_os) :
+                print()
+                # TODO: O_ij가 ms에 따라 상대적 할당될 시 pTime-> real_ms[0], 절대적 할당경로 -> real_ms[1]
+
+
+        fitness += self.machine[o][real_ms[0]]
+        real_pTime[real_ms[1]] += self.machine[o][real_ms[0]]
+
         return fitness
 
     def selectMachine(self, operation) :
         alt = self.machine[operation]
 
-
     def globalSelectionInit(self) :
-        ms = []
-        ta = []
-        msSize = 0
-        for j in range(len(self.job)) :
-            for o in range(len(self.operation[j])) :
-                msSize += 1
-        for i in range(msSize) :
-            ms.append(0)
-            ta.append(0)
+        print()
+        # TODO : GS부
 
-        for i in range(msSize) :
-              available = []
-            for mac in self.machine :
-                available.append(mac[i]+ta[i])
-
+    def localSelectionInit(self):
+        print()
+        # TODO : ls부
 
     def evolution(self) :
-        for i in range(self.populationSize) :
-            ms = self.globalSelectionInit()
+        # TODO : Init부
+        # TODO : Crossover부
+        # TODO : Muatate부
 
+        print(self.getFitness([[4,1,2,2,4],[2,2,1,1,2]]))
 
 if __name__ == "__main__" :
     ga = GA(populationSize=20, selectionProbability=0.5, threshold=0.7, mutationProbability=0.1)

71d26a7e8b8c5f062fcf145488b8d36fc58856d7
Gwang Jong Ko <rhkswhdwkd@naver.com>
2020-09-08 01:23:23 +0900

    Add files via upload

--- code/CS/levy.py
+++ code/CS/levy.py
@@ -2,8 +2,11 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-def levy(mu, c, x) :
-    y = math.sqrt(0.5/(2*math.pi))*(math.e**(-c/(2*(x-mu)))/(x-mu)**(3/2))
+def levy(mu, c, x):
+    try:
+        y = math.sqrt(0.5/(2*math.pi))*(math.e**(-c/(2*(x-mu)))/(x-mu)**(3/2))
+    except:
+        y = 0
     return y
 
 if __name__ == "__main__" :
@@ -13,16 +16,20 @@ if __name__ == "__main__" :
     yList1 = []
     yList2 = []
     yList3 = []
+    yList4 = []
 
-    for i in np.arange(0.001, 1.000, 0.001) :
+    for i in np.arange(0, 2, 0.001) :
         xList.append(i)
         yList1.append(levy(mu, 0.5, i))
         yList2.append(levy(mu, 1, i))
         yList3.append(levy(mu, 2, i))
+        yList4.append(levy(mu, 4, i))
 
     plt.plot(xList, yList1)
     plt.plot(xList, yList2)
     plt.plot(xList, yList3)
+    plt.plot(xList, yList4)
+    plt.legend(["c=0.5", "c=1", "c=2", "c=4"])
     plt.show()
 
 
